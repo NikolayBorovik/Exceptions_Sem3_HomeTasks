71003a1f381301efc00a6ecaf194b747de4547c1
Nikolay Borovik <nik.Borovik@bk.ru>
2023-07-30 20:33:41 +1000

    done

--- User/UserType.java
+++
@@ -1,7 +0,0 @@
-package Exceptions_Sem3_HomeTasks.User;
-
-public class UserType extends User {
-    public UserType(String firstName, String lastName, String patronimic, String db, int phoneNum, String sex) {
-        super(firstName, lastName, patronimic, db, phoneNum, sex);
-    }
-}
